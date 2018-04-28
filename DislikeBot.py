9312dce64818fccd93bf37c0c4e7cd203be65708
Rettend <36823200+Rettend@users.noreply.github.com>
2018-04-28 11:08:51 +0200

    Add files via upload

--- DislikeBot.py
+++ DislikeBot.py
@@ -9,6 +9,12 @@ import datetime
 import traceback
 import os
 import sys
+from ext import embedtobox
+import psutil
+import random
+import pip
+import json
+import io
 
 description = "The Offical bot of Like-Server!"
 bot = commands.Bot(command_prefix='d-', description=description)
@@ -30,7 +36,47 @@ async def on_message(message):
         await bot.send_message(message.channel, '**%s**' % (' '.join(args[1:])))
 bot.process_commands(message)
 
+class Information:
+    def __init__(self, bot):
+        self.bot = bot
 
+    @commands.command(no_pm=True)
+    async def channels(self, ctx, serverid:int = None):
+
+        if serverid is None:
+            server = ctx.guild
+        else:
+            server = discord.utils.get(self.bot.guilds, id=serverid)
+            if server is None:
+                return await ctx.send('Server not found!')
+
+        e = discord.Embed()
+        e.color = await ctx.get_dominant_color()
+
+        voice = ''
+        text = ''
+        categories = ''
+
+        for channel in server.voice_channels:
+            voice += f'\U0001f508 {channel}\n'
+        for channel in server.categories:
+            categories += f'\U0001f4da {channel}\n'
+        for channel in server.text_channels:
+            text += f'\U0001f4dd {channel}\n'
+        
+        if len(server.text_channels) > 0:
+            e.add_field(name='Text Channels', value=f'```{text}```')
+        if len(server.categories) > 0:
+            e.add_field(name='Categories', value=f'```{categories}```')
+        if len(server.voice_channels) > 0:
+            e.add_field(name='Voice Channels', value=f'```{voice}```')
+
+        try:
+            await ctx.send(embed=e)
+        except discord.HTTPException:
+            em_list = await embedtobox.etb(e)
+            for page in em_list:
+                await ctx.send(page)
 
 
     
